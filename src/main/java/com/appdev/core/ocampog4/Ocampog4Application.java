ab583e6074a612c4521e583baaa6b005c0a3fa2c
bacrilog <daniellemaxineocampo@gmail.com>
2025-10-23 02:55:39 +0800

    initial setup commit

---
+++ src/main/java/com/appdev/core/ocampog4/Ocampog4Application.java
@@ -0,0 +1,13 @@
+package com.appdev.core.ocampog4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ocampog4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ocampog4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdev/core/ocampog4/Ocampog4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.core.ocampog4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ocampog4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
